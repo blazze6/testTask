c3743d3ee0e3fe2e1737b7b3acdf439c30792582
Roman Karpov <blazze6@gmail.com>
2018-05-09 14:12:07 +0700

    Описана основная логика изменения массива

--- build/js/cards.js
+++ build/js/cards.js
@@ -1,13 +1,26 @@
-"use strict";
+'use strict';
 
-var cards = [
-    {
-        type: 'narrow'
-    },
-    {
-        type: 'wide'
-    },
-    {
-        type: 'narrow'
-    }
-];
+var cardNarrow = { type: 'narrow' };
+var cardWide = { type: 'wide' };
+
+var cardsDeck = document.querySelector('.cards');
+
+var checkClick = function(event){
+	if(event.altKey === true && event.shiftKey === true)
+		{
+			cards.push(cardWide);
+			console.log(cards);
+		}
+	else if(event.altKey === false && event.shiftKey === true)
+		{
+			cards.push(cardNarrow);
+			console.log(cards);
+		}
+	else
+		{
+			cards.pop();
+			console.log(cards);
+		}
+};
+
+cardsDeck.addEventListener('click', checkClick);
