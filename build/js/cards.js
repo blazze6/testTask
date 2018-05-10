36d65ff6abc83e44e67e4d62b8b90bb6d38baf62
Roman Karpov <blazze6@gmail.com>
2018-05-10 13:44:09 +0700

    Подключен Handlebars, добавление и удаление карточек на странице и правки респонсива

--- build/js/cards.js
+++ build/js/cards.js
@@ -1,26 +1,50 @@
 'use strict';
 
-var cardNarrow = { type: 'narrow' };
-var cardWide = { type: 'wide' };
-
+// Выбор контейнера с карточками
 var cardsDeck = document.querySelector('.cards');
 
+// Функция пересчёта исходного кол-ва карточек
+var countCards = function(){
+	for (var i = 0; i < cards.length; i++){
+	cards[i].count = i + 1;
+	}
+};
+
+// Посчитали карточки и каждому объекту массива добавили параметр count с порядковым номером
+countCards();
+
+// Выбор шаблона Handlebars и его компиляция с данными массива cards
+var cardsTemplate = document.getElementById('cards-template');
+var sourceContent = cardsTemplate.innerHTML;
+var compiledTemplate = Handlebars.compile(sourceContent);
+var mainContent = compiledTemplate({ allCards: cards });
+
+// Вывод в HTML данных скомпилированного шаблона
+cardsDeck.innerHTML = mainContent;
+
+// Функция, отслеживающая нажатие кнопок Alt и Shift в момент события (клика по контейнеру с карточками)
 var checkClick = function(event){
 	if(event.altKey === true && event.shiftKey === true)
 		{
-			cards.push(cardWide);
-			console.log(cards);
+			cards.push({ type: 'wide' });
+			cards[cards.length - 1].count = cards.length;
+			mainContent = compiledTemplate({ allCards: cards });
+			cardsDeck.innerHTML = mainContent;
 		}
 	else if(event.altKey === false && event.shiftKey === true)
 		{
-			cards.push(cardNarrow);
-			console.log(cards);
+			cards.push({ type: 'narrow' });
+			cards[cards.length - 1].count = cards.length;
+			mainContent = compiledTemplate({ allCards: cards });
+			cardsDeck.innerHTML = mainContent;
 		}
 	else
 		{
 			cards.pop();
-			console.log(cards);
+			mainContent = compiledTemplate({ allCards: cards });
+			cardsDeck.innerHTML = mainContent;
 		}
 };
 
+// Добавление обработчика события (клика по контейнеру с карточками)
 cardsDeck.addEventListener('click', checkClick);
